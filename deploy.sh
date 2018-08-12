028075c7bb68f447d29ae838d8acc6f83b20db81
zjhch123 <416836508@qq.com>
2018-08-12 19:48:20 +0800

    fix: router -> hash, update travis.yml

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 source /etc/profile
 cnpm install
 cnpm run build
+rm -rf /var/www/Statics/freelancer
+mv ./dist /var/www/Statics/freelancer
--- src/router.js
+++ src/router.js
@@ -10,7 +10,7 @@ import Me_Profile from './views/Me/Profile.vue'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
   routes: [
     {
