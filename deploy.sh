cb0ba56b4314195e3a678281131f76e958b0050f
zjhch123 <416836508@qq.com>
2018-08-20 15:10:56 +0800

    feat: 更新自动部署脚本

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 source /etc/profile
 cnpm install
 cnpm run build
-mv ./dist/* /var/node_server/freelancer-server/app/public/
+cd /var/node_server/freelancer-server/app/public/
+rm -rf `ls | egrep -v upload`
+mv /var/packages/freelancer/dist/* /var/node_server/freelancer-server/app/public/
 cd /var/node_server/freelancer-server/
 npm run stop
 npm run start
