1f27caf286a5187c7b4af3b7439a749a84b57a49
zjhch123 <416836508@qq.com>
2018-08-22 14:03:22 +0800

    fix: 兼容返回值null时的情况

--- src/views/Me/Profile.vue
+++ src/views/Me/Profile.vue
@@ -6,13 +6,13 @@
         <SimpleRow name="个人介绍：">{{person.introduction}}</SimpleRow>
         <SimpleRow name="常住城市：">{{detail.city}}</SimpleRow>
         <SimpleRow name="院校专业：">{{person.school}} {{person.grade}} {{person.major}}</SimpleRow>
-        <SimpleRow name="主要职业：">{{person.jobs.join(' ')}}</SimpleRow>
+        <SimpleRow name="主要职业：">{{(person.jobs || []).join(' ')}}</SimpleRow>
         <SimpleRow name="从事职业：">{{detail.job}}</SimpleRow>
       </div>
       <div class="m-info">
-        <SimpleRow name="代表作品：">{{person.productions.join(' ')}}</SimpleRow>
+        <SimpleRow name="代表作品：">{{(person.productions || []).join(' ')}}</SimpleRow>
         <SimpleRow name="主要作品：">
-          <span v-for="(p, i) in detail.production.split('\n')" :key="i">
+          <span v-for="(p, i) in (detail.production || '').split('\n')" :key="i">
             {{p}}<br/>
           </span>
         </SimpleRow>
